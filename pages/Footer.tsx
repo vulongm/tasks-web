a84bbded3c8bb3d4ad63382e513b7cf7ba3af0f4
Long Vu <hi@vulongm.com>
2022-07-15 02:17:47 +0000

    Add hover animation to footer

--- pages/Footer.tsx
+++ pages/Footer.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 const Footer: NextPage = () => {
   return (
     <footer className="text-black dark:text-white text-center lg:text-center p-3 bg-gray-200 dark:bg-gray-700">
-      Made by <a href="https://vulongm.com" className="p-1 rounded text-black bg-fuchsia-300">Long</a>
+      Made by <a href="https://vulongm.com" className="p-1 rounded text-black bg-fuchsia-300 hover:bg-fuchsia-200">Long</a>
     </footer>
   )
 }
