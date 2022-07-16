59cf0eee1c1e4bec63fe5a872f51e9356cff7d2c
Long Vu <hi@vulongm.com>
2022-07-15 20:02:40 -0400

    Add theme switcher

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Footer from './Footer'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen dark:bg-gray-800">
       <Head>
         <title>To Do</title>
         <meta name="description" content="A to do app!" />
