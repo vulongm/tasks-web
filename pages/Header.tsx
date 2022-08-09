94f2a90fd231cd55769666799e3869ad74bead6d
Long Vu <hi@vulongm.com>
2022-08-09 20:53:20 +0000

    Rename header text to Tasks and remove theme switcher

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -1,31 +1,13 @@
 import type { NextPage } from 'next'
 import { PlusIcon, CogIcon } from '@heroicons/react/outline'
-import { useState, useEffect } from 'react'
-import {useTheme} from 'next-themes'
 
 const Header: NextPage = () => {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) {
-    return null
-  }
-
   return (
     <nav className="flex items-center justify-between flex-wrap p-3 text-black bg-fuchsia-300">
       <h1 className="text-3xl font-bold">
-        To Do
+        Tasks
       </h1>
       <div className="space-x-2">
-        <select value={theme} onChange={e => setTheme(e.target.value)} className="inline-block h-7 bg-white dark:bg-white text-black dark:text-black">
-          <option value="system" className="bg-white dark:bg-white text-black dark:text-black">System</option>
-          <option value="dark" className="bg-white dark:bg-white text-black dark:text-black">Dark</option>
-          <option value="light" className="bg-white dark:bg-white text-black dark:text-black">Light</option>
-        </select>
         <PlusIcon className="inline-block h-9 w-9 p-2 border rounded border-black hover:bg-fuchsia-200"/>
         <CogIcon className="inline-block h-9 w-9 p-2 border rounded border-black hover:bg-fuchsia-200"/>
       </div>
