306a8647763aaacc6e77b8a48a063f306fae22a7
Long Vu <hi@vulongm.com>
2022-08-10 04:17:04 +0000

    Lighter border, array for notes, and add new note input

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -5,7 +5,7 @@ const Settings: NextPage = () => {
   return (
     <main className="flex-grow container mx-auto p-5">
       <h1 className="text-xl mb-3 dark:text-white">Settings</h1>
-      <div className="block p-5 mb-5 rounded border bg-white dark:bg-gray-800 border-black dark:border-white">
+      <div className="block p-5 mb-5 rounded border bg-white dark:bg-gray-800 border-gray-500 dark:border-gray-500">
         <h5 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">Theme</h5>
         <p className="mb-2 text-gray-700 dark:text-gray-400">Used to set between system, light, and dark mode.</p>
         <ThemeChanger/>
--- components/Tasks.tsx
+++ components/Tasks.tsx
@@ -1,42 +1,34 @@
 import type { NextPage } from 'next'
-import { PencilIcon, CheckIcon, XIcon } from '@heroicons/react/outline'
+import { PlusIcon, PencilIcon, XIcon } from '@heroicons/react/outline'
 
 const Tasks: NextPage = () => {
+  let tasks: String[] = ['Test']
+
+  const handleKeyDown = (e: { target: { style: { height: string }; scrollHeight: any } }) => {
+    e.target.style.height = "inherit";
+    e.target.style.height = `${e.target.scrollHeight}px`;
+  }
+
   return (
-    <main className="flex-grow container mx-auto p-5">
-      <h1 className="text-xl mb-3 dark:text-white">Active Tasks</h1>
-      <div className="block p-5 mb-5 rounded border bg-white dark:bg-gray-800 border-black dark:border-white">
+    <div className="flex-grow container mx-auto p-5">
+      <form className="p-2 block mb-5 rounded border bg-white dark:bg-gray-800 border-gray-500 dark:border-gray-500">
         <div className="flex justify-between items-center">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Eat the grape</h5>
+          <textarea className="flex-1 w-full pl-2 mr-2 text-black dark:text-white bg-white dark:bg-gray-800 hover:border-none dark:hover:border-none" placeholder="Enter task here" onKeyDown={handleKeyDown}/>
+          <PlusIcon className="h-9 w-9 p-2 border rounded dark:text-white border-black dark:border-white hover:bg-gray-300 dark:hover:bg-gray-500"/>
+        </div>
+      </form>
+      {tasks && tasks.map((note, index) =>
+        <div className="block p-2 mb-5 rounded border bg-white dark:bg-gray-800 border-gray-500 dark:border-gray-500" key={index}>
           <div className="flex justify-between items-center">
-            <PencilIcon className="h-9 w-9 p-2 mr-2 border rounded dark:text-white border-black dark:border-white hover:bg-gray-300 dark:hover:bg-gray-500"/>
-            <CheckIcon className="h-9 w-9 p-2 mr-2 border rounded dark:text-white border-black dark:border-white hover:bg-gray-300 dark:hover:bg-gray-500"/>
-            <XIcon className="h-9 w-9 p-2 border rounded dark:text-white border-black dark:border-white hover:bg-gray-300 dark:hover:bg-gray-500"/>
+            <p className="pl-2 text-black dark:text-white">{tasks}</p>
+            <div className="pl-2 flex justify-between items-center">
+              <PencilIcon className="h-9 w-9 p-2 mr-2 border rounded dark:text-white border-black dark:border-white hover:bg-gray-300 dark:hover:bg-gray-500"/>
+              <XIcon className="h-9 w-9 p-2 border rounded dark:text-white border-black dark:border-white hover:bg-gray-300 dark:hover:bg-gray-500"/>
+            </div>
           </div>
         </div>
-        <div className="mb-1">
-          <p className="p-1 mr-1 text-sm inline rounded text-black bg-orange-300">Food</p>
-          <p className="p-1 mr-1 text-sm inline rounded text-black bg-blue-300">Fruit</p>
-        </div>
-        <p className="text-gray-700 dark:text-gray-400">There is a grape in the living room.</p>
-      </div>
-      <div className="block p-5 mb-5 rounded border bg-white dark:bg-gray-800 border-black dark:border-white">
-        <div className="flex justify-between items-center">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Cook dinner</h5>
-          <div className="flex justify-between items-center">
-            <PencilIcon className="h-9 w-9 p-2 mr-2 border rounded dark:text-white border-black dark:border-white hover:bg-gray-300 dark:hover:bg-gray-500"/>
-            <CheckIcon className="h-9 w-9 p-2 mr-2 border rounded dark:text-white border-black dark:border-white hover:bg-gray-300 dark:hover:bg-gray-500"/>
-            <XIcon className="h-9 w-9 p-2 border rounded dark:text-white border-black dark:border-white hover:bg-gray-300 dark:hover:bg-gray-500"/>
-          </div>
-        </div>
-        <p className="mb-1 dark:text-white">Due by 12:00 AM</p>
-        <div className="mb-1">
-          <p className="p-1 mr-1 text-sm inline rounded text-black bg-orange-300">Food</p>
-          <p className="p-1 mr-1 text-sm inline rounded text-black bg-green-300">Dinner</p>
-        </div>
-        <p className="text-gray-700 dark:text-gray-400">There is leftover spaghetti in the fridge.</p>
-      </div>
-    </main>
+      )}
+    </div>
   )
 }
 
